121e8f33603a7c77a191e1690377f2f891a13d47
陈辉 <chenhui@rock-chips.com>
2012-04-02 15:28:59 +0800

    enable Google Market, and update Play to 3.5.15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,7 +48,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 BOARD_LIB_DUMPSTATE := libdumpstate.$(TARGET_BOARD_PLATFORM)
 
 # google apps
-#BUILD_WITH_GOOGLE_MARKET := true
+BUILD_WITH_GOOGLE_MARKET := true
 
 # face lock
 #BUILD_WITH_FACELOCK := true
