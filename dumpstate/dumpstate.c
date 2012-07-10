2f07b9cc681270405b8b33d97db2c10ec3e3c89d
陈辉 <chenhui@rock-chips.com>
2012-07-10 23:20:58 +0800

    fix compile error:
    device/rockchip/rk30sdk/dumpstate/dumpstate.c:1:23: fatal error: dumpstate.h: No such file or directory

--- dumpstate/dumpstate.c
+++
@@ -1,9 +0,0 @@
-#include <dumpstate.h>
-
-void dumpstate_board()
-{
-    dump_file("clocks", "/proc/clocks");
-    dump_file("rk29xxnand", "/proc/rk29xxnand");
-    dump_file("vpu mem", "/proc/vpu_mem");
-    dump_file("vpu service", "/proc/vpu_service");
-};
