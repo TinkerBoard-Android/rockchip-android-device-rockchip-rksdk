512fe075bedbba561ebcbbb2bd0461fe3d444c2b
chenhui <chenhui@rock-chips.com>
2012-07-11 15:42:00 +0800

    fix compile error: make: *** No rule to make target out/target/product/rk30sdk/obj/SHARED_LIBRARIES/libMali_intermediates/export_includes needed by out/target/product/rk30sdk/obj/SHARED_LIBRARIES/gralloc.rk30board_intermediates/import_includes

--- device.mk
+++ device.mk
@@ -121,17 +121,17 @@ PRODUCT_COPY_FILES += \
         device/rockchip/rk30sdk/rk30xxnand_ko.ko.3.0.8+:root/rk30xxnand_ko.ko.3.0.8+ 
 PRODUCT_COPY_FILES += \
        device/rockchip/rk30sdk/vold.fstab:system/etc/vold.fstab 
-        
+
+# GPU-MALI        
+PRODUCT_PACKAGES += \
+        libEGL_mali.so \
+        libGLESv1_CM_mali.so \
+        libGLESv2_mali.so \
+        libMali.so \
+        libUMP.so \
+        mali.ko \
+        ump.ko 
 PRODUCT_COPY_FILES += \
-        device/rockchip/rk30sdk/proprietary/libmali/libMali.so:system/lib/libMali.so \
-        device/rockchip/rk30sdk/proprietary/libmali/libMali.so:obj/lib/libMali.so \
-        device/rockchip/rk30sdk/proprietary/libmali/libUMP.so:system/lib/libUMP.so \
-        device/rockchip/rk30sdk/proprietary/libmali/libUMP.so:obj/lib/libUMP.so \
-        device/rockchip/rk30sdk/proprietary/libmali/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
-        device/rockchip/rk30sdk/proprietary/libmali/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
-        device/rockchip/rk30sdk/proprietary/libmali/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
-        device/rockchip/rk30sdk/proprietary/libmali/mali.ko:system/lib/modules/mali.ko \
-        device/rockchip/rk30sdk/proprietary/libmali/ump.ko:system/lib/modules/ump.ko \
         device/rockchip/rk30sdk/gpu_performance/performance_info.xml:system/etc/performance_info.xml \
         device/rockchip/rk30sdk/gpu_performance/performance:system/bin/performance \
         device/rockchip/rk30sdk/gpu_performance/libperformance_runtime.so:system/lib/libperformance_runtime.so \
