4c10c6e036c8a0f826a8fea06c83af88b6791d03
csy <csy@rock-chips.com>
2012-07-14 15:50:19 +0800

    merge sf4.0.4 change to sf4.1.1 must sync frameworks/av

--- device.mk
+++ device.mk
@@ -182,6 +182,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
         device/rockchip/rk30sdk/proprietary/libvpu/media_codecs.xml:system/etc/media_codecs.xml \
+	device/rockchip/rk30sdk/proprietary/libvpu/registry:system/lib/registry \
         device/rockchip/rk30sdk/proprietary/libvpu/vpu_service.ko:system/lib/modules/vpu_service.ko
 
 ifeq ($(strip $(BUILD_WITH_RK_EBOOK)),true)
