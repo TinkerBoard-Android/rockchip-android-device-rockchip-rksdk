c7a28f1142eb64a2f35878e835117c16c5f66370
chm <chm@rock-chips.com>
2012-07-16 10:14:22 +0800

    remove redundant lib define

--- proprietary/libvpu/Android.mk
+++ proprietary/libvpu/Android.mk
@@ -33,22 +33,17 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_MULTI_PREBUILT)
   
 include $(CLEAR_VARS)
+LOCAL_PREBUILT_LIBS := librkswscale.so               
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_MULTI_PREBUILT)
+  
+include $(CLEAR_VARS)
 LOCAL_PREBUILT_LIBS := librkwmapro.so                
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_MULTI_PREBUILT)
   
 include $(CLEAR_VARS)
 LOCAL_PREBUILT_LIBS := libvpu.so                
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := libjpeghwenc.so
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_LIBS := libjpeghwdec.so
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_MULTI_PREBUILT)
 
