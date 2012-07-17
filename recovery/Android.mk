176980fd64fa724c410734c092bc9be5d296c02f
陈辉 <chenhui@rock-chips.com>
2012-07-17 17:46:34 +0800

    add rk recovery on JB

---
+++ recovery/Android.mk
@@ -0,0 +1,15 @@
+ifeq ($(TARGET_DEVICE),rk29sdk)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := eng
+LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_SRC_FILES := recovery_ui.cpp
+
+# should match TARGET_RECOVERY_UI_LIB set in BoardConfig.mk
+LOCAL_MODULE := librecovery_ui_rk29sdk
+
+include $(BUILD_STATIC_LIBRARY)
+
+endif
