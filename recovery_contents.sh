c240b7a25bc79113224f8125ad179ec6b6629a4e
mmk <mmk@rock-chips.com>
2013-03-07 16:58:40 +0800

    copy sdtool busybox from common

--- recovery_contents.sh
+++ recovery_contents.sh
@@ -7,8 +7,8 @@ PCBA_PATH=external/rk-pcba-test
 ############################################################################################
 #rk recovery contents
 ############################################################################################
-cp -f device/rockchip/$TARGET_PRODUCT/sdtool $PRODUCT_OUT/recovery/root/sbin/
-cp -f device/rockchip/$TARGET_PRODUCT/proprietary/bin/busybox $PRODUCT_OUT/recovery/root/sbin/
+cp -f device/rockchip/common/bin/sdtool $PRODUCT_OUT/recovery/root/sbin/
+cp -f device/rockchip/common/bin/busybox $PRODUCT_OUT/recovery/root/sbin/
 cp -f $PRODUCT_OUT/obj/EXECUTABLES/codec_test_intermediates/codec_test $PRODUCT_OUT/recovery/root/sbin/
 cp -f $PRODUCT_OUT/obj/EXECUTABLES/pcba_core_intermediates/pcba_core $PRODUCT_OUT/recovery/root/sbin/
 ############################################################################
