234c0b812f7f7eaa72e51d41aa026fbc208eb9de
hwg <hwg@rock-chips.com>
2013-04-01 16:19:57 +0800

    add wifi_bt.mk for wifi bt config

---
+++ wifi_bt.mk
@@ -0,0 +1,14 @@
+#
+# wifi bt chip config
+#
+
+# broadcom
+BROADCOM_WIFI_SUPPORT := true
+BROADCOM_BT_SUPPORT   := true
+
+# mt5931 && mt6622
+MT5931_WIFI_SUPPORT   := false
+MT6622_BT_SUPPORT     := false
+
+
+
