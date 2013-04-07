845ab5b5381a2a873824a2d57f2add66797d2372
hwg <hwg@rock-chips.com>
2013-04-07 17:27:40 +0800

    bluetooth: revert broadcom bplus

--- wifi_bt.mk
+++ wifi_bt.mk
@@ -3,12 +3,12 @@
 #
 
 # broadcom
-BROADCOM_WIFI_SUPPORT := true
-BROADCOM_BT_SUPPORT   := true
+BROADCOM_WIFI_SUPPORT := false
+BROADCOM_BT_SUPPORT   := false
 
 # mt5931 && mt6622
-MT5931_WIFI_SUPPORT   := false
-MT6622_BT_SUPPORT     := false
+MT5931_WIFI_SUPPORT   := true
+MT6622_BT_SUPPORT     := true
 
 
 
