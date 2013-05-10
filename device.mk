e73db19f0cb912dc37800ee9bdb23dc92bb55cf8
Cody Xie <xkd@rock-chips.com>
2013-05-10 17:28:29 +0800

    add properties for screen solution higher than 1920x1200

--- device.mk
+++ device.mk
@@ -27,6 +27,23 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(strip $(BOARD_USE_LCDC_COMPOSER)), true)
 include frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.stack-trace-file=/data/anr/traces.txt \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.path_cache_size=16 \
+    ro.hwui.shape_cache_size=4 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flush_rate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024 \
+    ro.hwui.disable_scissor_opt=true
+
 else
 ifeq ($(strip $(BOARD_USE_LOW_MEM)), true)
 include frameworks/native/build/tablet-dalvik-heap.mk
