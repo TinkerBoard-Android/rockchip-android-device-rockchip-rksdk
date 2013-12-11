e990955f96e01c95f62d629b53b82e4138b3eea8
hwg <hwg@rock-chips.com>
2013-12-11 13:12:04 +0800

    update wifi_bt_build.sh, reduce make

--- wifi_bt_build.sh
+++ wifi_bt_build.sh
@@ -7,14 +7,18 @@ croot
 echo "---- make installclean ----"
 make installclean
 
-echo "---- make -j4 ----"
-make -j4
-
 echo "---- mmm ----"
 mmm external/wpa_supplicant_8/ -B
 mmm external/bluetooth/bluedroid/ -B
 mmm device/common/bluetooth/libbt/ -B
 mmm hardware/libhardware_legacy/ -B
 mmm system/netd/ -B
-mmm frameworks/base/core/res/ -B
-mmm frameworks/base/ -B
+touch frameworks/base/wifi/java/android/net/wifi/*
+touch frameworks/base/wifi_old/java/android/net/wifi/*
+touch frameworks/base/core/jni/android_net_wifi_WifiNative_old.cpp
+touch frameworks/base/core/jni/android_net_wifi_WifiNative.cpp
+mmm frameworks/base/
+
+echo "---- make -j4 ----"
+make -j4
+
