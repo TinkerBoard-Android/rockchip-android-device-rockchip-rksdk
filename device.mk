9390f2ed42e288af5f880db7d186ff5cc4f0f60e
cw <cw@rock-chips.com>
2013-12-14 16:47:29 +0800

    Fix issue that zygote die once on boot caused by wrong environment variable

--- device.mk
+++ device.mk
@@ -273,11 +273,18 @@ ifeq ($(strip $(BUILD_WITH_UMS)),true)
 		ro.factory.hasUMS=true \
 		persist.sys.usb.config=mass_storage,adb 
        		#testing.mediascanner.skiplist = /mnt/internal_sd/Android/
+
+
+	PRODUCT_COPY_FILES += \
+    		device/rockchip/rksdk/init.$(TARGET_BOARD_HARDWARE).hasUMS.true.rc:root/init.$(TARGET_BOARD_HARDWARE).environment.rc
 else
 	PRODUCT_PROPERTY_OVERRIDES += \
 		ro.factory.hasUMS=false \
 		persist.sys.usb.config=mtp,adb 
        		#testing.mediascanner.skiplist = /mnt/shell/emulated/Android/
+
+        PRODUCT_COPY_FILES += \
+                device/rockchip/rksdk/init.$(TARGET_BOARD_HARDWARE).hasUMS.false.rc:root/init.$(TARGET_BOARD_HARDWARE).environment.rc
 endif
 
 ########################################################
