0324106c63ce65a083813acb146bebe5f3ae33d4
cw <cw@rock-chips.com>
2014-01-13 10:26:15 +0800

    Remove *.prop in BoardConfig is WRONG, temporary discard it

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,12 +5,10 @@
 
 #DO CLEAN
 ifdef PRODUCT_OUT
-    $(shell rm -f $(PRODUCT_OUT)/root/default.prop)
     $(shell rm -rf $(PRODUCT_OUT)/system/preinstall)
     $(shell rm -rf $(PRODUCT_OUT)/system/preinstall_del)
 endif
 ifdef TARGET_OUT
-    $(shell rm -f $(TARGET_OUT)/build.prop)
     $(shell rm -f $(TARGET_OUT)/manifest.xml)
 endif
 
