bf61c51a6f9d3cb8cfc7cee083723c4c09995bbb
cw <cw@rock-chips.com>
2014-01-16 16:35:59 +0800

    Discard cleaning in prebuild.mk

--- prebuild.mk
+++ prebuild.mk
@@ -1,14 +1,3 @@
-#DO CLEAN
-ifdef PRODUCT_OUT
-    $(shell rm -f $(PRODUCT_OUT)/root/default.prop)
-    $(shell rm -rf $(PRODUCT_OUT)/system/preinstall)
-    $(shell rm -rf $(PRODUCT_OUT)/system/preinstall_del)
-endif
-ifdef TARGET_OUT
-    $(shell rm -f $(TARGET_OUT)/build.prop)
-    $(shell rm -f $(TARGET_OUT)/manifest.xml)
-endif
-
 #GENERATE MANIFEST
 $(shell test -d .repo && .repo/repo/repo manifest -r -o manifest.xml)
 
