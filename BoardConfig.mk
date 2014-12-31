21b447a4c1c781a3d620303b3d1beb7b3f92a018
zhanghao <zhanghao@rock-chips.com>
2014-12-31 11:49:01 +0800

    Merge arm & x86 platform into rockchip/common
    1.set default value :  BOARD_HAVE_BLUETOOTH_BCM ?= false

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,6 +163,7 @@ BOARD_USES_GENERIC_AUDIO ?= true
 # Wifi&Bluetooth
 BOARD_HAVE_BLUETOOTH ?= true
 BLUETOOTH_USE_BPLUS ?= false
+BOARD_HAVE_BLUETOOTH_BCM ?= false
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/rockchip/$(TARGET_PRODUCT)/bluetooth
 -include device/rockchip/$(TARGET_PRODUCT)/wifi_bt.mk
 include device/rockchip/common/wifi_bt_common.mk
