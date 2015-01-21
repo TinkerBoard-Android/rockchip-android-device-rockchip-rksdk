b79334f29b2c843dc11b607c1b6254575bb51db0
zhanghao <zhanghao@rock-chips.com>
2015-01-21 12:33:05 +0800

    recovery : recovery_contents.sh cp sbin from different ARCH.  please update build folder first.

--- recovery_contents.sh
+++ recovery_contents.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-echo "$1 $2 $3 $4"
+echo "$1 $2 $3 $4 $5"
 TARGET_PRODUCT=$1
 PRODUCT_OUT=$2
 TARGET_BOARD_PLATFORM=$3
 TARGET_ROCKCHIP_PCBATEST=$4
+TARGET_ARCH=$5
 
 PCBA_PATH=external/rk-pcba-test
 
 ############################################################################################
 #rk recovery contents
 ############################################################################################
-cp -f vendor/rockchip/common/bin/arm/sdtool $PRODUCT_OUT/recovery/root/sbin/
-cp -f vendor/rockchip/common/bin/arm/busybox $PRODUCT_OUT/recovery/root/sbin/
+cp -f vendor/rockchip/common/bin/$TARGET_ARCH/sdtool $PRODUCT_OUT/recovery/root/sbin/
+cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/sbin/
 
 if [ $TARGET_ROCKCHIP_PCBATEST = "true" ];then
 cp -f $PRODUCT_OUT/obj/EXECUTABLES/codec_test_intermediates/codec_test $PRODUCT_OUT/recovery/root/sbin/
