5d1331d7bc8bbd92e61af134f7a60bbcd2977499
zhanghao <zhanghao@rock-chips.com>
2015-01-26 10:18:27 +0800

    recovery : disable console if not debuggable

--- recovery_contents.sh
+++ recovery_contents.sh
@@ -11,7 +11,6 @@ PCBA_PATH=external/rk-pcba-test
 ############################################################################################
 #rk recovery contents
 ############################################################################################
-cp -f vendor/rockchip/common/bin/$TARGET_ARCH/sdtool $PRODUCT_OUT/recovery/root/sbin/
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/sbin/
 
 if [ $TARGET_ROCKCHIP_PCBATEST = "true" ];then
