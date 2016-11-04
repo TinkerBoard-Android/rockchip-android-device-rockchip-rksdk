8d49b001c2c0a013ac0f0639915a2c30d485b0cd
Andy Ye <Andy.Ye@rock-chips.com>
2016-11-04 09:23:51 +0800

    sepolicy:init for nougat
    Change-Id: I8e487e3ccdac42957e0e9c38687531db4b057695

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,7 @@ ART_USE_HSPACE_COMPACT ?= true
 TARGET_USES_LOGD ?= true
 
 # Sepolicy
-#BOARD_SEPOLICY_DIRS ?= device/rockchip/common/sepolicy
+BOARD_SEPOLICY_DIRS ?= device/rockchip/common/sepolicy
 
 # Recovery
 TARGET_NO_RECOVERY ?= false
