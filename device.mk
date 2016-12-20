612feab8531aa283b58ef328b1138f9fcca34cb7
huweiguo <hwg@rock-chips.com>
2016-12-20 14:18:03 +0800

    bluetooth: support realtek bt
    Change-Id: Ifdd0f0a8a84baadbb37d1cefd0d57edaf9cab95f

--- device.mk
+++ device.mk
@@ -485,10 +485,13 @@ endif
 
 # for realtek bluetooth
 PRODUCT_PACKAGES += \
+    bluetooth_rtk.default \
     libbt-vendor.so \
     libbt-vendor_uart.so \
     libbt-vendor_usb.so \
     bt_vendor.conf
+#include hardware/realtek/rtkbt/rtkbt.mk
+$(call inherit-product, hardware/realtek/rtkbt/rtkbt.mk)
 
 ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
 include device/rockchip/common/samba/rk31_samba.mk
