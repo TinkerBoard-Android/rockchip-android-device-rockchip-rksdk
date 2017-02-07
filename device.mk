5b12d79c36b26e890850dba28103d3342fce80e3
sxj <sxj@rock-chips.com>
2017-02-07 16:14:57 +0800

    device.mk: rk3188 support iep.
    Change-Id: I00f4bd8ddf20169eeba60ad5fc92622c07aa6ac3
    Signed-off-by: sxj <sxj@rock-chips.com>

--- device.mk
+++ device.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
     akmd 
 
 # iep
-ifneq ($(filter rk3190 rk3026 rk3288 rk312x rk3368 rk3328 rk3366 rk3399, $(strip $(TARGET_BOARD_PLATFORM))), )
+ifneq ($(filter rk3188 rk3190 rk3026 rk3288 rk312x rk3368 rk3328 rk3366 rk3399, $(strip $(TARGET_BOARD_PLATFORM))), )
 BUILD_IEP := true
 PRODUCT_PACKAGES += \
     libiep
