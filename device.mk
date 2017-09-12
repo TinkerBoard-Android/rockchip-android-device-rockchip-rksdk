8b31c6699b474663eba25b4f01277a7f22fa7126
zhangyunlong <dalon.zhang@rock-chips.com>
2017-09-12 10:26:55 +0800

    Oreo: add camera support
    Change-Id: If58d33929f4a1e4ccceb05dc45076b986764f1c8
    Signed-off-by: zhangyunlong <dalon.zhang@rock-chips.com>

--- device.mk
+++ device.mk
@@ -318,6 +318,14 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     hwcomposer.$(TARGET_BOARD_HARDWARE) \
 
+# Camera HAL
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.metadata@3.2 \
+    android.hardware.camera.provider@2.4-service
+
 # iep
 ifneq ($(filter rk3188 rk3190 rk3026 rk3288 rk312x rk3126c rk3128 px3se rk3368 rk3328 rk3366 rk3399, $(strip $(TARGET_BOARD_PLATFORM))), )
 BUILD_IEP := true
