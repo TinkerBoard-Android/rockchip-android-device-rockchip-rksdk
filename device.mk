2e0f3934fcb376d1177e8c437b901386f040d241
Hao Xiaowei <hxw@rock-chips.com>
2017-10-25 17:56:28 +0800

    audio : Replace audio policy configure as xml file for vts
    Change-Id: I82fa74c456d0e678f238efb7a3c69b058d54a3fe
    Signed-off-by: Hao Xiaowei <hxw@rock-chips.com>

--- device.mk
+++ device.mk
@@ -151,7 +151,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio_policy_$(PRODUCT_BUILD_MODULE).conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
 else
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio_policy_$(TARGET_BOARD_HARDWARE).conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
+    $(LOCAL_PATH)/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio_policy_volumes_drc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes_drc.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 endif
 
 
