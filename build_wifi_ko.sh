54716623b99952c903bebcd60ec522df3bda6f39
Alex Zhao <zzc@rock-chips.com>
2017-11-06 16:32:11 +0800

    Ensure /vendor/lib/modules directory exists
    Change-Id: I5926347543f42e2a5b2de2e05c3d928adb14608b
    Signed-off-by: Alex Zhao <zzc@rock-chips.com>

--- build_wifi_ko.sh
+++ build_wifi_ko.sh
@@ -6,5 +6,7 @@ echo "TARGET_ARCH = $TARGET_ARCH"
 
 make -C kernel ARCH=$TARGET_ARCH  modules -j8
 
+mkdir -p $TARGET_OUT_VENDOR/lib/modules
+
 find kernel/drivers/net/wireless/rockchip_wlan/*  -name "*.ko" | \
-xargs -n1 -i cp {} $TARGET_OUT_VENDOR/lib/modules
+xargs -n1 -i cp {} $TARGET_OUT_VENDOR/lib/modules/
