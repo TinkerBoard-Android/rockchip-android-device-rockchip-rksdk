dddd15880513f7bedd28a97cf0ad90a93a96c32f
Huacong Yang <will.yang@rock-chips.com>
2017-12-21 14:21:58 +0800

    recovery: remove copy newfs_msdos
    Change-Id: I482ecf4a7c1178ac3d6582d38d8cfa7f12020ad1

--- recovery_contents.sh
+++ recovery_contents.sh
@@ -16,7 +16,7 @@ cp -f vendor/rockchip/common/bin/$TARGET_ARCH/sh $PRODUCT_OUT/recovery/root/syst
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/system/bin/
 
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/sbin/
-cp -f vendor/rockchip/common/bin/$TARGET_ARCH/newfs_msdos $PRODUCT_OUT/recovery/root/sbin/
+#cp -f vendor/rockchip/common/bin/$TARGET_ARCH/newfs_msdos $PRODUCT_OUT/recovery/root/sbin/
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/sh $PRODUCT_OUT/recovery/root/sbin/
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/e2fsck $PRODUCT_OUT/recovery/root/sbin/
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/resize2fs $PRODUCT_OUT/recovery/root/sbin/
