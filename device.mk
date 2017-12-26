efe275cff3503a24af70a2d724104540ce9d1d52
Hao Xiaowei <hxw@rock-chips.com>
2017-12-26 14:50:37 +0800

    Add MediaCasService for rk platform to fix related cts failures
    Change-Id: I6e17dff0906a66b28c9def33a1b6de3d38ff9448
    Signed-off-by: Hao Xiaowei <hxw@rock-chips.com>

--- device.mk
+++ device.mk
@@ -367,6 +367,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PACKAGES += \
+    libclearkeycasplugin \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-impl
 
