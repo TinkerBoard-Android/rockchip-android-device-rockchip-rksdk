da235a59dda0bc3101b7751b387a74c8cca86412
Bian Jin chen <kenjc.bian@rock-chips.com>
2018-06-14 14:34:39 +0800

    Android Go: Remove telephone for tablet.
    Change-Id: If4d9f4e490fef1799e087e1d645f38024c370aa4
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), atv)
 else ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
   $(call inherit-product, device/rockchip/common/tv/tv_base.mk)
 else ifeq ($(strip $(BUILD_WITH_GO_OPT))|$(strip $(TARGET_ARCH)) ,true|arm)
-  $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
   $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
   $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackageGo.mk)
 else
