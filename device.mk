1bf6a4449aa11d1c48c54a46200ea09a8f5e61a2
huangjc <huangjc@rock-chips.com>
2018-07-31 18:26:02 +0800

    audio:fix for audio V4
    Change-Id: Ic69d790ec82bdb75f0b1db0b40a76f64dc9ce106

--- device.mk
+++ device.mk
@@ -391,7 +391,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-service \
+    android.hardware.audio@2.0-service \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.0-impl
