a6225bb0bb19795fb9dbf58e43d9de9fa7bf6792
Zhen Chen <chenzhen@rock-chips.com>
2018-09-04 17:46:26 +0800

    property_contexts: for VTS, remove entries of unused hwui properties
    Change-Id: I28688d11434fe52fc95a9a67770ac3ed6a82df37

--- device.mk
+++ device.mk
@@ -102,18 +102,6 @@ ifeq ($(strip $(BOARD_USE_LCDC_COMPOSER)), true)
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024 \
-    ro.hwui.disable_scissor_opt=true \
     ro.rk.screenshot_enable=true   \
     sys.status.hidebar_enable=false \
     persist.sys.ui.hw=true
@@ -828,18 +816,6 @@ endif
 
 #add for hwui property
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024 \
-    ro.hwui.disable_scissor_opt=true \
     ro.rk.screenshot_enable=true   \
     ro.rk.hdmi_enable=true   \
     sys.status.hidebar_enable=false
