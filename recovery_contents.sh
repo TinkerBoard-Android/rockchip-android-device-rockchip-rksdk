dcbfa669b59b0a18481e3f4baec5ee88c2f8e13f
Zorro Liu <lyx@rock-chips.com>
2018-09-20 10:12:58 +0800

    recovery_contents.sh: modify PCBA_PATH and copy files to vendor dir instead of system dir
    Change-Id: I7c47f8c65b7d0336f5473d6ac88696a4f2b561d4
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- recovery_contents.sh
+++ recovery_contents.sh
@@ -6,14 +6,14 @@ TARGET_BOARD_PLATFORM=$3
 TARGET_ROCKCHIP_PCBATEST=$4
 TARGET_ARCH=$5
 
-PCBA_PATH=external/rk-pcba-test
+PCBA_PATH=external/pcba_rockchip/pcba_core
 
 ############################################################################################
 #rk recovery contents
 ############################################################################################
-mkdir -p $PRODUCT_OUT/recovery/root/system/bin/
-cp -f vendor/rockchip/common/bin/$TARGET_ARCH/sh $PRODUCT_OUT/recovery/root/system/bin/
-cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/system/bin/
+mkdir -p $PRODUCT_OUT/recovery/root/vendor/bin
+cp -f vendor/rockchip/common/bin/$TARGET_ARCH/sh $PRODUCT_OUT/recovery/root/vendor/bin/
+cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/vendor/bin/
 
 cp -f vendor/rockchip/common/bin/$TARGET_ARCH/busybox $PRODUCT_OUT/recovery/root/sbin/
 #cp -f vendor/rockchip/common/bin/$TARGET_ARCH/newfs_msdos $PRODUCT_OUT/recovery/root/sbin/
