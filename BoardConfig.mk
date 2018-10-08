6a4f6e1a8519064df99ea32eb772d355a874d843
Dayao Ji <jdy@rock-chips.com>
2018-10-08 10:13:03 +0800

    Remove CMDLINE:skip_initramfs to resolve  many times ota upgrade
    Change-Id: I77ccc61f011995e6c766b4b9463712d5d29a4260
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,9 +64,9 @@ PRODUCT_SUPPORTS_BOOT_SIGNER := false
 BOARD_SELINUX_ENFORCING ?= false
 
 ifeq ($(filter true, $(BOARD_AVB_ENABLE)), )
-BOARD_KERNEL_CMDLINE ?= androidboot.wificountrycode=US androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init skip_initramfs rootwait ro init=/init
+BOARD_KERNEL_CMDLINE ?= androidboot.wificountrycode=US androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init rootwait ro init=/init
 else
-BOARD_KERNEL_CMDLINE ?= console=ttyFIQ0 androidboot.baseband=N/A androidboot.wificountrycode=US androidboot.veritymode=enforcing androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init skip_initramfs rootwait ro init=/init root=PARTUUID=af01642c-9b84-11e8-9b2a-234eb5e198a0
+BOARD_KERNEL_CMDLINE ?= console=ttyFIQ0 androidboot.baseband=N/A androidboot.wificountrycode=US androidboot.veritymode=enforcing androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init rootwait ro init=/init root=PARTUUID=af01642c-9b84-11e8-9b2a-234eb5e198a0
 endif
 BOARD_BOOTIMG_HEADER_VERSION ?= 1
 ROCKCHIP_RECOVERYIMAGE_CMDLINE_ARGS ?= console=ttyFIQ0 androidboot.baseband=N/A androidboot.selinux=permissive androidboot.wificountrycode=US androidboot.veritymode=enforcing androidboot.hardware=rk30board androidboot.console=ttyFIQ0 firmware_class.path=/vendor/etc/firmware init=/init root=PARTUUID=af01642c-9b84-11e8-9b2a-234eb5e198a0
