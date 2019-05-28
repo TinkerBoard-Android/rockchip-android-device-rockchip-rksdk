55f891a77ff91921e8330c5d780b5fb5e4b62d3e
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-05-28 11:30:32 +0800

    Android: add path for creating link between odm and vendor/odm.
    Test: VtsVndkDependency#testElfDependency
    Error: error in 'adb pull /odm /tmp/tmpxcCwa', no such file or
    dictionary.
    
    Change-Id: I4fe0c8e083b60180178a29b5c5ff3c8ae7d4f9be
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 $(shell mkdir -p $(TARGET_ROOT_OUT)/mnt/vendor/metadata)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/app)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/bin)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/etc)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/firmware)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/framework)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/lib)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/lib64)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/overlay)
+$(shell mkdir -p $(TARGET_OUT_VENDOR)/odm/priv-app)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
