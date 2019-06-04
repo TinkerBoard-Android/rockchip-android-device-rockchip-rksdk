5568c9147908033d35797467e874babd8d8b88a1
huangjc <huangjc@rock-chips.com>
2019-06-04 17:57:02 +0800

    rootservice:update for arm64
    redmine:208044
    
    Change-Id: I2db5e9db1ef4c65d5d3a8e724e76c117ac3145d9

--- rootservice/rootservice.mk
+++ rootservice/rootservice.mk
@@ -5,7 +5,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(CUR_PATH)/bin/rootservice:/vendor/bin/rootservice \
     $(CUR_PATH)/bin/rootsudaemon.sh:/vendor/bin/rootsudaemon.sh \
-    $(CUR_PATH)/xbin/daemonsu:/system/xbin/daemonsu \
-    $(CUR_PATH)/xbin/su:/system/xbin/su \
-    $(CUR_PATH)/xbin/supolicy:/system/xbin/supolicy \
-    $(CUR_PATH)/lib/libsupol.so:/system/lib/libsupol.so
+    $(CUR_PATH)/$(TARGET_ARCH)/xbin/daemonsu:/system/xbin/daemonsu \
+    $(CUR_PATH)/$(TARGET_ARCH)/xbin/su:/system/xbin/su \
+    $(CUR_PATH)/$(TARGET_ARCH)/xbin/supolicy:/system/xbin/supolicy
+
+ifeq ($(strip $(TARGET_ARCH)), arm64)
+PRODUCT_COPY_FILES += \
+    $(CUR_PATH)/$(TARGET_ARCH)/lib64/libsupol.so:/system/lib64/libsupol.so
+else
+PRODUCT_COPY_FILES += \
+    $(CUR_PATH)/$(TARGET_ARCH)/lib/libsupol.so:/system/lib/libsupol.so
+endif
