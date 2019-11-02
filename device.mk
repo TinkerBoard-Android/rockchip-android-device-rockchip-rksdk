9ff9bae11695613ac67f8402ef8a3182fd6f82e3
Bian Jin chen <kenjc.bian@rock-chips.com>
2019-11-02 17:24:59 +0800

    device: support wifi passpoint feature for GTS.
    Test:
        GtsGmscoreHostTestCases
        com.google.android.gts.wifi.WifiHostTest#testPasspointCapability
    
    Change-Id: I2d680174b6f67885a1157b07492705eae4050805
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>

--- device.mk
+++ device.mk
@@ -259,7 +259,8 @@ endif
 ifeq ($(BOARD_WIFI_SUPPORT),true)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
 endif
 
 ifeq ($(BOARD_HAS_GPS),true)
