017507b8e460de9097f54792da82f8d15f49ad18
吴良清 <wlq@rock-chips.com>
2020-04-17 14:29:45 +0800

    Revert "hidl: add  android.hardware.configstore"
    This reverts commit 10a6db3b051ac8ca40f885ed9b5079c512b46220.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I144a768013599cd769220ebcf50ae802fae437a4

--- device.mk
+++ device.mk
@@ -420,11 +420,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     sensors.$(TARGET_BOARD_HARDWARE)
 
-
-# ConfigStoreHAL, deprecated, but still needs to be installed in Q.
-PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-service
-
 endif
 
 # Power HAL
