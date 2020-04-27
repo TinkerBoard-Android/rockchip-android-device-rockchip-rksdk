5b65eac2faab2e8ad1d6f92f2acebec2cd923254
Wu Liangqing <wlq@rock-chips.com>
2020-04-27 17:15:58 +0800

    Revert "lollipop migrate : add board HAL dumpstate and healthd"
    This reverts commit b44606598a6363e1361735ad947d74f49d89d84a.
    
    Change-Id: Ifc39e7038ae342f350c6fe8662bb9844cf3d7aab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -214,7 +214,6 @@ endif
 #endif
 TARGET_RELEASETOOLS_EXTENSIONS := device/rockchip/common
 TARGET_PROVIDES_INIT_RC ?= false
-#BOARD_HAL_STATIC_LIBRARIES ?= libdumpstate.$(TARGET_PRODUCT) libhealthd.$(TARGET_PRODUCT)
 
 //MAX-SIZE=512M, for generate out/.../system.img
 BOARD_FLASH_BLOCK_SIZE := 131072
