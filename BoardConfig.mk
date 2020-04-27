574d3ba2ed3078256498bdd800972251ed75decf
Wu Liangqing <wlq@rock-chips.com>
2020-04-27 17:06:20 +0800

    Revert "Add support for boot boost"
    This reverts commit aac4822989f7eecaa733dd10a7b8ff25e97ccc04.
    
    Change-Id: If7e2447f295e53f576488ffb4ef931c2753df01e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -366,9 +366,6 @@ BOARD_CHARGER_DISABLE_INIT_BLANK ?= true
 #stress test
 BOARD_HAS_STRESSTEST_APP ?= true
 
-#boot optimization
-BOARD_WITH_BOOT_BOOST ?= false
-
 #optimise mem
 BOARD_WITH_MEM_OPTIMISE ?= false
 
--- device.mk
+++ device.mk
@@ -949,16 +949,6 @@ endif
 PRODUCT_PACKAGES += \
 	abc
 
-# boot optimization
-PRODUCT_COPY_FILES += \
-        device/rockchip/common/boot_boost/libboot_optimization.so:system/lib/libboot_optimization.so
-ifeq ($(strip $(BOARD_WITH_BOOT_BOOST)),true)
-PRODUCT_COPY_FILES += \
-        device/rockchip/common/boot_boost/prescan_packages.xml:system/etc/prescan_packages.xml
-PRODUCT_PROPERTY_OVERRIDES += \
-        ro.boot_boost.enable=true
-endif
-
 ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), vr)
 PRODUCT_COPY_FILES += \
        device/rockchip/common/lowmem_package_filter.xml:system/etc/lowmem_package_filter.xml 
