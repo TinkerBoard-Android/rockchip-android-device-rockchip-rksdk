54bf2115cfa8015881366b3d762efed49ca6994a
Dayao Ji <jdy@rock-chips.com>
2020-10-10 16:41:44 +0800

    Support Non OTA build and standalone rkloader package for A/B
    Signed-off-by: Dayao Ji <jdy@rock-chips.com>
    Change-Id: Ic5a248e4b115d3ed2df1d508b4c58a3003bf5f20

--- mkimage_ab.sh
+++ mkimage_ab.sh
@@ -15,7 +15,7 @@ TARGET_BASE_PARAMETER_IMAGE=`get_build_var TARGET_BASE_PARAMETER_IMAGE`
 BOARD_AVB_ENABLE=`get_build_var BOARD_AVB_ENABLE`
 BOARD_KERNEL_CMDLINE=`get_build_var BOARD_KERNEL_CMDLINE`
 ROCKCHIP_RECOVERYIMAGE_CMDLINE_ARGS=`get_build_var ROCKCHIP_RECOVERYIMAGE_CMDLINE_ARGS`
-BOARD_BOOTIMG_HEADER_VERSION=`get_build_var BOARD_BOOTIMG_HEADER_VERSION`
+BOARD_BOOTIMG_HEADER_VERSION=`get_build_var BOARD_BOOT_HEADER_VERSION`
 PRODUCT_USE_DYNAMIC_PARTITIONS=`get_build_var PRODUCT_USE_DYNAMIC_PARTITIONS`
 BOARD_USES_AB_IMAGE=`get_build_var BOARD_USES_AB_IMAGE`
 BOARD_USES_AB_LEGACY_RETROFIT=`get_build_var BOARD_USES_AB_LEGACY_RETROFIT`
@@ -29,6 +29,7 @@ echo BOARD_USES_AB_LEGACY_RETROFIT=$BOARD_USES_AB_LEGACY_RETROFIT
 echo BOARD_USES_RECOVERY_AS_BOOT=$BOARD_USES_RECOVERY_AS_BOOT
 echo PRODUCT_RETROFIT_DYNAMIC_PARTITIONS=$PRODUCT_RETROFIT_DYNAMIC_PARTITIONS
 echo PRODUCT_USE_DYNAMIC_PARTITIONS=$PRODUCT_USE_DYNAMIC_PARTITIONS
+echo BOARD_BOOTIMG_HEADER_VERSION=$BOARD_BOOTIMG_HEADER_VERSION
 TARGET="withoutkernel"
 if [ "$1"x != ""x  ]; then
          TARGET=$1
@@ -54,7 +55,8 @@ echo system filesysystem is $FSTYPE
 BOARD_CONFIG=device/rockchip/common/device.mk
 
 PARAMETER=${TARGET_DEVICE_DIR}/parameter_ab.txt
-FLASH_CONFIG_FILE=${TARGET_DEVICE_DIR}/config.cfg
+FLASH_CONFIG_FILE=${TARGET_DEVICE_DIR}/config.cfg_ab
+FLASH_CONFIG_FILE_GKI=${TARGET_DEVICE_DIR}/config.cfg_ab_gki
 
 KERNEL_SRC_PATH=`grep TARGET_PREBUILT_KERNEL ${BOARD_CONFIG} |grep "^\s*TARGET_PREBUILT_KERNEL *:= *[\w]*\s" |awk  '{print $3}'`
 
@@ -81,6 +83,15 @@ BOOT_OTA="ota"
         fi
     fi
 
+copy_images_from_out() {
+if [ ! -f "$OUT/$1" ]; then
+    echo "skip copy images: $1"
+else
+    echo "create $1..."
+    cp -a $OUT/$1 $IMAGE_PATH/$1
+    echo "done."
+fi
+}
 
 echo "create dtbo.img.... "
 if [ ! -f "$OUT/dtbo.img" ]; then
@@ -91,35 +102,25 @@ fi
 cp -a $BOARD_DTBO_IMG $IMAGE_PATH/dtbo.img
 echo "done."
 
-echo "create boot.img.... "
-if [ "$BOARD_AVB_ENABLE" = "true" ]; then
-cp -a $OUT/boot.img $IMAGE_PATH/boot.img
-else
-echo "BOARD_AVB_ENABLE is false, make boot.img from kernel && out."
-    [ -d $OUT/recovery/root ] && \
-    mkbootfs -d $OUT/system $OUT/recovery/root | minigzip > $OUT/ramdisk-recovery.img && \
-    mkbootimg --kernel $KERNEL_DEBUG --ramdisk $OUT/ramdisk-recovery.img --second kernel/resource.img --os_version $PLATFORM_VERSION --header_version $BOARD_BOOTIMG_HEADER_VERSION --os_patch_level $PLATFORM_SECURITY_PATCH --cmdline "$ROCKCHIP_RECOVERYIMAGE_CMDLINE_ARGS" --output $OUT/boot.img && \
-    cp -a $OUT/boot.img $IMAGE_PATH/boot.img
+echo "create resource.img..."
+if [ -f "kernel/resource.img" ]; then
+    cp -a kernel/resource.img $IMAGE_PATH/resource.img
+    echo "done."
 fi
-echo "done."
 
-echo -n "create system.img.... "
-if [ "$BOARD_AVB_ENABLE" = "true" ]; then
-echo -n "system.img has been signed by avbtool, just copy."
-else
-echo "BOARD_AVB_ENABLE is false, make system.img from out."
-    [ -d $OUT/system ] && \
-    python build/make/tools/releasetools/build_image.py \
-    $OUT/system \
-    $OUT/obj/PACKAGING/systemimage_intermediates/system_image_info.txt \
-    $OUT/system.img \
-    $OUT/system
+copy_images_from_out boot.img
+copy_images_from_out boot-debug.img
+copy_images_from_out vendor_boot.img
+copy_images_from_out vendor_boot-debug.img
+copy_images_from_out super.img
+
+if [ ! "$PRODUCT_USE_DYNAMIC_PARTITIONS" = "true" ]; then
+copy_images_from_out system.img
+copy_images_from_out system_ext.img
+copy_images_from_out vendor.img
+copy_images_from_out odm.img
+copy_images_from_out product.img
 fi
-python device/rockchip/common/sparse_tool.py $OUT/system.img
-mv $OUT/system.img.out $OUT/system.img
-cp -f $OUT/system.img $IMAGE_PATH/system.img
-#cp -f $OUT/system.img $IMAGE_PATH/system.img
-echo "done."
 
 echo -n "create vbmeta.img.... "
 if [ "$BOARD_AVB_ENABLE" = "true" ]; then
@@ -129,40 +130,6 @@ echo -n "BOARD_AVB_ENABLE is false,use default vbmeta.img"
 cp -a device/rockchip/common/vbmeta.img $IMAGE_PATH/vbmeta.img
 fi
 echo -n "done."
-
-echo -n "create vendor.img..."
-if [ "$BOARD_AVB_ENABLE" = "true" ]; then
-echo -n "vendor.img has been signed by avbtool, just copy."
-else
-echo "BOARD_AVB_ENABLE is false, make vendor.img from out."
-    [ -d $OUT/vendor ] && \
-    python build/make/tools/releasetools/build_image.py \
-    $OUT/vendor \
-    $OUT/obj/PACKAGING/vendor_intermediates/vendor_image_info.txt \
-    $OUT/vendor.img \
-    $OUT/system
-fi
-python device/rockchip/common/sparse_tool.py $OUT/vendor.img
-mv $OUT/vendor.img.out $OUT/vendor.img
-cp -a $OUT/vendor.img $IMAGE_PATH/vendor.img
-echo -n "done."
-
-echo -n "create odm.img..."
-if [ "$BOARD_AVB_ENABLE" = "true" ]; then
-echo -n "odm.img has been signed by avbtool, just copy."
-else
-echo "BOARD_AVB_ENABLE is false, make odm.img from out."
-    [ -d $OUT/odm ] && \
-    python build/make/tools/releasetools/build_image.py \
-    $OUT/odm \
-    $OUT/obj/PACKAGING/odm_intermediates/odm_image_info.txt \
-    $OUT/odm.img \
-    $OUT/system
-fi
-python device/rockchip/common/sparse_tool.py $OUT/odm.img
-mv $OUT/odm.img.out $OUT/odm.img
-cp -f $OUT/odm.img $IMAGE_PATH/odm.img
-echo "done."
 
 if [ "$PRODUCT_USE_DYNAMIC_PARTITIONS" = "true" ]; then
     if [ "$PRODUCT_RETROFIT_DYNAMIC_PARTITIONS" = "true" ]; then
@@ -227,23 +194,6 @@ else
 	fi
 fi
 
-if [ -f $KERNEL_PATH/resource.img ]
-then
-        echo -n "create resource.img..."
-        cp -a $KERNEL_PATH/resource.img $IMAGE_PATH/resource.img
-        echo "done."
-else
-        echo "$KERNEL_PATH/resource.img not fount!"
-fi
-
-if [ -f $KERNEL_PATH/kernel.img ]
-then
-        echo -n "create kernel.img..."
-        cp -a $KERNEL_PATH/kernel.img $IMAGE_PATH/kernel.img
-        echo "done."
-else
-        echo "$KERNEL_PATH/kernel.img not fount!"
-fi
 
 if [ -f $FLASH_CONFIG_FILE ]
 then
@@ -254,14 +204,39 @@ else
     echo "$FLASH_CONFIG_FILE not fount!"
 fi
 
+if [ $BOARD_BOOTIMG_HEADER_VERSION -ge 3 ] ; then
+    echo "Boot header version is >=3 "
+    if [ -f $FLASH_CONFIG_FILE_GKI ] ; then
+        cp -a $FLASH_CONFIG_FILE_GKI $IMAGE_PATH/config.cfg
+    fi
+else
+    echo "Boot header version is < 3 "
+fi
+
 if [ "$BOARD_USES_AB_IMAGE" = "true" ]; then
-	echo "BOARD_USES_AB_IMAGE is true... "
-	if [ -f ${TARGET_DEVICE_DIR}/parameter_ab.txt ]; then
-		cp -a ${TARGET_DEVICE_DIR}/parameter_ab.txt $IMAGE_PATH/parameter.txt
-		echo "done."
-  else
-		echo "${TARGET_DEVICE_DIR}/parameter_ab.txt not fount! Please make it from ${TARGET_DEVICE_DIR} first!"
-  fi
+    echo "BOARD_USES_AB_IMAGE is true... "
+    if [ -f ${TARGET_DEVICE_DIR}/parameter_ab.txt ]; then
+        cp -a ${TARGET_DEVICE_DIR}/parameter_ab.txt $IMAGE_PATH/parameter.txt
+        echo "done."
+    else
+        if [ -f $OUT/parameter.txt ]; then
+            cp -a $OUT/parameter.txt $IMAGE_PATH/parameter.txt
+        else
+            echo "$PARAMETER not fount!"
+        fi
+    fi
+fi
+
+SHARED_LIBRARIES_DIR=out/host/linux-x86/lib64
+JAVA_LIBRARIES_DIR=out/host/linux-x86/framework
+OTA_KEY_DIR=build/target/product/security
+
+if [ $TARGET == $BOOT_OTA ]; then
+    echo "create update_loader.zip.."
+    python build/tools/releasetools/package_loader_zip.py  $IMAGE_PATH/MiniLoaderAll.bin  $IMAGE_PATH/update_loader_unsigned.zip
+    java -Djava.library.path=$SHARED_LIBRARIES_DIR -jar $JAVA_LIBRARIES_DIR/signapk.jar -w $OTA_KEY_DIR/testkey.x509.pem $OTA_KEY_DIR/testkey.pk8 $IMAGE_PATH/update_loader_unsigned.zip $IMAGE_PATH/update_loader.zip
+    rm $IMAGE_PATH/update_loader_unsigned.zip
+    echo "done."
 fi
 
 if [ "$TARGET_BASE_PARAMETER_IMAGE"x != ""x ]
