5a21d9455dc474e304c02f8fa32efde96d95caaf
wengtao <tao.weng@rock-chips.com>
2021-06-16 20:16:32 +0800

    mk: low_mem: Remove unused configs
    remove unused configs & add go_defaults_common.mk configs.
    
    Signed-off-by: wengtao <tao.weng@rock-chips.com>
    Change-Id: I1e305c866aca62332f3ee6232116825edbdea78d

--- tv/tv_low_ram_device.mk
+++ tv/tv_low_ram_device.mk
@@ -1,3 +1,4 @@
+# include target/product/go_defaults_common.mk
 ifneq ($(filter atv box, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.mem_optimise.enable=true \
@@ -11,13 +12,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
     pm.dexopt.shared=quicken \
     persist.traced.enable=1 \
     dalvik.vm.madvise-random=true \
-    ro.lmk.medium=700
+    ro.lmk.medium=700 \
+    dalvik.vm.heapgrowthlimit=128m \
+    dalvik.vm.heapsize=256m
 PRODUCT_COPY_FILES += \
     device/rockchip/common/lowmem_package_filter.xml:system/etc/lowmem_package_filter.xml
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-PRODUCT_PACKAGES += InProcessNetworkStack
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD :=false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_DISABLE_SCUDO := true
+TARGET_VNDK_USE_CORE_VARIANT := true
 endif
