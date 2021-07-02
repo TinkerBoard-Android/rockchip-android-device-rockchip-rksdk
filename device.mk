ff689cf38c4ea1ee72a807e066747434c35edfbb
Bian Jin chen <kenjc.bian@rock-chips.com>
2021-07-02 08:50:48 +0000

    manifests: upgrading audio HAL to 7.0
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I7e93442d976a8687487814fae144e52e18ac3052

--- device.mk
+++ device.mk
@@ -539,8 +539,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl
 
 PRODUCT_PACKAGES += \
     libclearkeycasplugin
