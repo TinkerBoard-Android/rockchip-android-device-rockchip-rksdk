53fc5567ede7c0a35bbd1ea1b14881f36a8be5c3
Bian Jin chen <kenjc.bian@rock-chips.com>
2022-03-30 17:17:15 +0800

    bootanim: change the target path to the system partition
    Android 12+ does not allow bootanim to read files from odm,
    We recommand using apex bootanimation here.
    
    Test: add /odm/media/bootanimation.zip to user build.
    
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I6d372cc6ff8fe61b0bd9fc681e194448066b60fb

--- bootshutdown/bootshutdown.mk
+++ bootshutdown/bootshutdown.mk
@@ -4,8 +4,8 @@ HAVE_BOOT_ANIMATION := $(shell test -f $(CUR_PATH)/bootanimation.zip && echo yes
 HAVE_SHUTDOWN_ANIMATION := $(shell test -f $(CUR_PATH)/shutdownanimation.zip && echo yes)
 
 ifeq ($(HAVE_BOOT_ANIMATION), yes)
-PRODUCT_COPY_FILES += $(CUR_PATH)/bootanimation.zip:$(TARGET_COPY_OUT_ODM)/media/bootanimation.zip
+PRODUCT_COPY_FILES += $(CUR_PATH)/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 endif
 ifeq ($(HAVE_SHUTDOWN_ANIMATION), yes)
-PRODUCT_COPY_FILES += $(CUR_PATH)/shutdownanimation.zip:$(TARGET_COPY_OUT_ODM)/media/shutdownanimation.zip
+PRODUCT_COPY_FILES += $(CUR_PATH)/shutdownanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/shutdownanimation.zip
 endif
