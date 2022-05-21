285fb716aaf50b9cbd8605050891f5307820c30b
Wu Liangqing <wlq@rock-chips.com>
2022-05-21 09:26:12 +0000

    build.sh: add clang build config for arm
    Change-Id: I0c96f21236bdf3d4a16e1966e8daab2fd7d11f86

--- build/rockchip/build.sh
+++ build/rockchip/build.sh
@@ -140,9 +140,12 @@ fi
 fi
 
 if [ "$BUILD_KERNEL_WITH_CLANG" = true ] ; then
-ADDON_ARGS="CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
+if [ "$KERNEL_ARCH" = "arm64" ]; then
+    ADDON_ARGS="CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
+else
+    ADDON_ARGS="CC=clang LD=ld.lld"
 fi
-
+fi
 # build kernel
 if [ "$BUILD_KERNEL" = true ] ; then
 echo "Start build kernel"
