a5c77c302aa2b06528edc1af9c28104c910d5cfb
callen.cai <callen.cai@rock-chips.com>
2022-05-27 11:02:50 +0800

    media_drm : add widevie L1 ta.
    Signed-off-by: callen.cai <callen.cai@rock-chips.com>
    Change-Id: Ie6847db14c28b128bea429bf2257f895a01cd042

--- modules/media_drm.mk
+++ modules/media_drm.mk
@@ -41,4 +41,15 @@ else
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.widevine
 endif
+ifeq ($(BOARD_WIDEVINE_OEMCRYPTO_LEVEL), 1)
+    PRODUCT_PACKAGES += \
+        liboemcrypto
+    ifneq ($(filter rk3326 rk356x rk3588, $(strip $(TARGET_BOARD_PLATFORM))), )
+        PRODUCT_PACKAGES += \
+            c11fe8ac-b997-48cf-a28d-e2a55e5240ef.ta
+    else
+        PRODUCT_PACKAGES += \
+            c11fe8ac-b997-48cf-a28de2a55e5240ef.ta
+    endif
+endif # Widevine L1
 endif
