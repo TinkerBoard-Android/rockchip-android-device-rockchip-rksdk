77daea28b81ee637e558a0575ce23cab93cb3258
Bian Jin chen <kenjc.bian@rock-chips.com>
2022-07-08 08:11:04 +0000

    Change health HAL to health AIDL.
    TODO: Develop health AIDL instead of demo code.
    
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I9297420829ad336d0938804a84c9bc784a005772

--- device.mk
+++ device.mk
@@ -461,8 +461,8 @@ PRODUCT_PACKAGES += \
 
 #Health hardware
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery
 
 # Filesystem management tools
 # EXT3/4 support
