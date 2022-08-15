cc4dc5ee654c4464f5a10716fb3d5759042a1afe
Luo Wei <lw@rock-chips.com>
2022-08-15 19:46:36 +0800

    fix build.sh building warning from "=="
    Signed-off-by: Luo Wei <lw@rock-chips.com>
    Change-Id: Ifa1b56c1889faf81070086a075b68704bdfcd882

--- build/rockchip/build.sh
+++ build/rockchip/build.sh
@@ -170,7 +170,7 @@ cd u-boot && ./scripts/pack_resource.sh ../$LOCAL_KERNEL_PATH/resource.img && cp
 
 IS_VEHICLE=`get_build_var BOARD_ROCKCHIP_VEHICLE`
 LOGO_VEHICLE_PATH=`get_build_var TARGET_DEVICE_DIR`
-if [ $IS_VEHICLE == "true" ]; then
+if [ $IS_VEHICLE = "true" ]; then
 ./$LOGO_VEHICLE_PATH/pack_resource.sh ./$LOCAL_KERNEL_PATH/resource.img && cp resource.img ./$LOCAL_KERNEL_PATH/resource.img
 fi
 
