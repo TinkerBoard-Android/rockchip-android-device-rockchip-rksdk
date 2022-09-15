e9e1c2036390dc58a68cc3d235b0104b89596567
Luo Wei <lw@rock-chips.com>
2022-09-15 17:15:11 +0800

    car: change the directory of ueventd.rc
    Signed-off-by: Luo Wei <lw@rock-chips.com>
    Change-Id: I69d2800d2901ff60d82f937a4377034dce15d1b6

--- rootdir/rootdir.mk
+++ rootdir/rootdir.mk
@@ -25,7 +25,15 @@ PRODUCT_COPY_FILES += \
     $(ROCKCHIP_ROOT_DIR_PATH)/init.rk30board.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(TARGET_BOARD_HARDWARE).rc \
     $(ROCKCHIP_ROOT_DIR_PATH)/init.rk30board.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(TARGET_BOARD_HARDWARE).usb.rc \
     $(ROCKCHIP_ROOT_DIR_PATH)/init.recovery.rk30board.rc:recovery/root/init.recovery.$(TARGET_BOARD_HARDWARE).rc \
-    $(ROCKCHIP_ROOT_DIR_PATH)/ueventd.rockchip.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
+
+
+ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), car)
+PRODUCT_COPY_FILES += \
+    device/rockchip/common/car/ueventd.rockchip.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+else
+PRODUCT_COPY_FILES += \
+    $(ROCKCHIP_ROOT_DIR_PATH)/ueventd.rockchip.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+endif
 
 PRODUCT_COPY_FILES += \
     $(ROCKCHIP_ROOT_DIR_PATH)/init.system.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.rockchip.rc
