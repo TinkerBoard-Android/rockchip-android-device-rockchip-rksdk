06ce27f20a1cb5b4f72f6c748fb6579c05719a80
wlq <wlq@rock-chips.com>
2022-09-17 07:46:26 +0000

    Revert "add picocom support"
    This reverts commit 278a1f69ac21e1c5955a84a304cbd850a823a5f6.
    
    Change-Id: Iade32be8e06fb5d97a9402c28638aedced242076

--- device.mk
+++ device.mk
@@ -887,6 +887,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.logd.kernel=1
+PRODUCT_COPY_FILES += \
+    device/rockchip/common/zmodem/rz:$(TARGET_COPY_OUT_VENDOR)/bin/rz \
+    device/rockchip/common/zmodem/sz:$(TARGET_COPY_OUT_VENDOR)/bin/sz
 PRODUCT_PACKAGES += io
 endif
 
