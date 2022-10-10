98665fa50bf9d63b70e7dc3a7b25d381015b84d3
Bian Jin chen <kenjc.bian@rock-chips.com>
2022-10-10 09:45:44 +0000

    avb: append avb footer for resource.img
    Signed-off-by: Bian Jin chen <kenjc.bian@rock-chips.com>
    Change-Id: I8a912b496f368f034bc7d3bcf1c92a3419351963

--- build/rockchip/RebuildParameter.mk
+++ build/rockchip/RebuildParameter.mk
@@ -11,7 +11,7 @@ endif # BOARD_USES_AB_IMAGE
 ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
 # Header V3, add vendor_boot and resource.
 ifeq (1,$(strip $(shell expr $(BOARD_BOOT_HEADER_VERSION) \>= 3)))
-partition_list := $(partition_list),resource_a:16M,vendor_boot_a:$(BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE)
+partition_list := $(partition_list),resource_a:$(BOARD_RESOURCEIMAGE_PARTITION_SIZE),vendor_boot_a:$(BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE)
 ifeq (1,$(strip $(shell expr $(BOARD_BOOT_HEADER_VERSION) \>= 4)))
 partition_list := $(partition_list),init_boot_a:$(BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE)
 endif # Header V4
@@ -20,7 +20,7 @@ partition_list := $(partition_list),dtbo_a:$(BOARD_DTBOIMG_PARTITION_SIZE),vbmet
 else # None-A/B
 # Header V3, add vendor_boot and resource.
 ifeq (1,$(strip $(shell expr $(BOARD_BOOT_HEADER_VERSION) \>= 3)))
-partition_list := $(partition_list),resource:16M,vendor_boot:$(BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE)
+partition_list := $(partition_list),resource:$(BOARD_RESOURCEIMAGE_PARTITION_SIZE),vendor_boot:$(BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE)
 ifeq (1,$(strip $(shell expr $(BOARD_BOOT_HEADER_VERSION) \>= 4)))
 partition_list := $(partition_list),init_boot:$(BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE)
 endif # Header V4
