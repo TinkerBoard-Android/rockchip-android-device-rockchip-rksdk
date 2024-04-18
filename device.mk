24b691f93a13dcf14c8f58846af2d2c77f0452e1
Chen Jinsen <kevin.chen@rock-chips.com>
2024-04-18 19:25:03 +0800

    rk3576: Add support scaling down 8K video playing
    Type: Feat
    Redmine ID: N/A
    Associated modifications: N/A
    Test: N/A
    
    Signed-off-by: Chen Jinsen <kevin.chen@rock-chips.com>
    Change-Id: I078404a0018adebd89e5329c1eea9bfa50922d7d

--- device.mk
+++ device.mk
@@ -854,8 +854,7 @@ ifneq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
 
     ifneq ($(filter rk3576, $(TARGET_BOARD_PLATFORM)), )
         PRODUCT_PROPERTY_OVERRIDES += \
-            rt_vdec_fbc_disable=1 \
-            codec2_fbc_disable=1
+            rt_vdec_fbc_min_stride=4096
     endif
 endif
 
